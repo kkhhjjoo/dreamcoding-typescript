8008b4f9f2bb5aacd5770dfef2baff133849f036
kkhhjjoo <khjoo1203@gmail.com>
2025-12-15 17:39:01 +0900

    함수에서 타입 이용하기(js->ts)

---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,25 @@
+{ 
+  //JavaScript💩
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+
+  //TypeScript🎇
+  function add(num1:number, num2:number):number {
+    return num1 + num2;
+  }
+
+  //JavaScript
+  // function jsFetchNum(id) {
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  //TypeScript
+  function fetchNum(id: string):Promise<number> {
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+}
